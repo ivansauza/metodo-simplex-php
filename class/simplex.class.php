5c58faf7c3300a59b3c72a3909e068ceacd4a080
Ivan Sauza <sauuza@gmail.com>
2018-06-20 22:33:18 -0500

    Solución inicial para empezar con la clase simplex.class.php

--- class/simplex.class.php
+++ class/simplex.class.php
@@ -4,5 +4,28 @@
  */
 class Simplex
 {
-	
+	protected $ob; // Objetivo del problema, minimzar o maximizar
+	protected $z;  // Valores de Z
+	protected $re; // Restricciones
+	protected $de; // Desigualdades o Igualdades de las restricciones
+	protected $fn; // Variable para decidir si se ah terminado el problema
+
+	protected $pivote;
+	protected $filaPivote;
+	protected $columnaPivote;
+
+
+	function __construct( $objetivo, $variables, $restricciones, $desigualdades )
+	{
+		$this->ob = $objetivo;
+		$this->z  = $variables;
+		$this->re = $restricciones;
+		$this->de = $desigualdades;
+		$this->fn = false;
+	}
+
+	public function execute()
+	{
+		
+	}
 }
