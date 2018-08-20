668a64ad5a58c0448a4096650225d6f159f39d3b
Ivan Sauza <sauuza@gmail.com>
2018-08-19 19:48:08 -0500

    Se agregaron las funciones que se encontraban en el archivo solution.php para ser mas ordenado, se mejoro codigo

--- helpers.php
+++ helpers.php
@@ -1,8 +1,73 @@
 <?php
-function dd( $var )
-{
-	echo "<pre><br />";
-	var_dump( $var );
-	echo "</pre>";
-}
+	function dd( $var )
+	{
+		echo "<pre><br />";
+		var_dump( $var );
+		echo "</pre>";
+	}
+
+	function getObjetivo()
+	{
+		return $_GET['objetivo'];
+	}
+
+	function getZ()
+	{
+		$temp = [];
+
+		for ( $i = 0; $i < $_GET['v']; $i ++ ) 
+		{ 
+			$temp[] = $_GET['x' . $i];
+		}
+
+		return $temp;
+	}
+
+	function getRestricciones()
+	{
+		$temp = [];
+
+		//Anadimos los arreglos deacuerdo a cuantas restricciones existen
+		for ( $i = 0; $i < $_GET['r']; $i ++ ) 
+		{ 
+			$temp[] = [];
+		}
+
+		//LLenamos las restricciones
+		for ( $i = 0; $i < $_GET['r']; $i ++ ) 
+		{ 
+			for ( $j = 0; $j < $_GET['v']; $j ++ ) 
+			{ 
+				$temp[$i][] = (int)$_GET['r' . $i . '_' . $j];
+			}
+
+			//Agregamos el valor final a la restriccion
+			//$temp[$i][] = (int)$_GET['s' . $i];
+		}
+		return $temp;
+	}
+
+	function getDesigualdades()
+	{
+		$temp = [];
+
+		for ( $i = 0; $i < $_GET['r']; $i ++ ) 
+		{ 
+			$temp[] = $_GET['d' . $i];
+		}
+
+		return $temp;
+	}
+
+	function getSoluciones()
+	{
+		$temp = [];
+
+		for ( $i = 0; $i < $_GET['r']; $i ++ ) 
+		{ 
+			$temp[] = $_GET['s' . $i];
+		}
+
+		return $temp;
+	}
 ?>
